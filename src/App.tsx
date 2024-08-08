207c4be9bcceae8f4a4926cb79837fba1216344b
Stanislav Kryshchuk <503282404@gehealthcare.com>
2024-08-08 23:14:15 +0300

    Implement Logo component in order to use it inside the Header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="container mx-auto">
-      <Header />
-    </div>
+    <>
+      <div className="container mx-auto">
+        <Header />
+      </div>
+      <hr />
+    </>
   );
 }
 
