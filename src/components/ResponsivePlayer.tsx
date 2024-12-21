7134586c230c29c620df763904027e95647146e2
Stanislav Kryshchuk <503282404@gehealthcare.com>
2024-12-21 21:32:16 +0200

    Add vimeo section video

---
+++ src/components/ResponsivePlayer.tsx
@@ -0,0 +1,17 @@
+import ReactPlayer from "react-player/vimeo";
+
+export default function ResponsivePlayer() {
+  return (
+    <div className="relative pt-[56.25%]">
+      <ReactPlayer
+        className="absolute top-0 left-0"
+        url="https://player.vimeo.com/video/1041410776?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+        width="100%"
+        height="100%"
+        controls
+        light
+        playing
+      />
+    </div>
+  );
+}
