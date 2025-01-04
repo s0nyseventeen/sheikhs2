b7bc82f9947d3daca0eda1ce502294a8d13a7ec8
Stanislav Kryshchuk <503282404@gehealthcare.com>
2025-01-04 22:43:06 +0200

    Add MagicPen component (svg icon) in order to use it in the Statistics component

---
+++ src/components/icons/MagicPen.tsx
@@ -0,0 +1,49 @@
+export default function MagicPen() {
+  return (
+    <span>
+      <svg
+        width="50"
+        height="50"
+        viewBox="0 0 50 50"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M7.29166 42.7083C9.02082 44.4375 11.8125 44.4375 13.5417 42.7083L40.625 15.625C42.3542 13.8958 42.3542 11.1042 40.625 9.375C38.8958 7.64583 36.1042 7.64583 34.375 9.375L7.29166 36.4583C5.56249 38.1875 5.56249 40.9792 7.29166 42.7083Z"
+          stroke="#292D32"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M37.5208 18.7291L31.2708 12.4791"
+          stroke="#292D32"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M17.7083 5.08329L20.8333 4.16663L19.9167 7.29163L20.8333 10.4166L17.7083 9.49996L14.5833 10.4166L15.5 7.29163L14.5833 4.16663L17.7083 5.08329Z"
+          stroke="#292D32"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M9.375 17.5833L12.5 16.6666L11.5833 19.7916L12.5 22.9166L9.375 22L6.25 22.9166L7.16667 19.7916L6.25 16.6666L9.375 17.5833Z"
+          stroke="#292D32"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M40.625 28L43.75 27.0834L42.8333 30.2084L43.75 33.3334L40.625 32.4167L37.5 33.3334L38.4167 30.2084L37.5 27.0834L40.625 28Z"
+          stroke="#292D32"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </span>
+  );
+}
