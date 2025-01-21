31b267d83f55dc3f49b2ab1b7d5ea1857dfdf871
Stanislav Kryshchuk <503282404@gehealthcare.com>
2025-01-21 11:00:49 +0200

    [components] Move horizontal line into the `Header` component in order to make `App` cleaner

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,34 +4,39 @@ import Logo from "./icons/Logo";
 
 export default function Header() {
   return (
-    <header className="container mx-auto">
-      <div className="flex justify-between items-center py-5 text-xl">
-        <a href="#" className="ml-5">
-          <Logo color={"#1C1C1C"} />
-        </a>
-        <nav>
-          <ul className="flex items-center gap-12">
-            {["Про шейхів", "Портфоліо", "Наші послуги"].map((link, index) => (
-              <li key={index}>
-                <a href="#" className="hover:text-gray-600 transition">
-                  {link}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <div>
-          <a
-            href="#"
-            className="flex items-center gap-1 hover:text-gray-600 transition"
-          >
-            <span>
-              <CiGlobe size={16} />
-            </span>
-            <span>UA</span>
+    <>
+      <header className="container mx-auto">
+        <div className="flex justify-between items-center py-5 text-xl">
+          <a href="#" className="ml-5">
+            <Logo color={"#1C1C1C"} />
           </a>
+          <nav>
+            <ul className="flex items-center gap-12">
+              {["Про шейхів", "Портфоліо", "Наші послуги"].map(
+                (link, index) => (
+                  <li key={index}>
+                    <a href="#" className="hover:text-gray-600 transition">
+                      {link}
+                    </a>
+                  </li>
+                ),
+              )}
+            </ul>
+          </nav>
+          <div>
+            <a
+              href="#"
+              className="flex items-center gap-1 hover:text-gray-600 transition"
+            >
+              <span>
+                <CiGlobe size={16} />
+              </span>
+              <span>UA</span>
+            </a>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+      <hr />
+    </>
   );
 }
